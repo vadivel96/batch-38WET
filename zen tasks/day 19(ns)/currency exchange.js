2448b4bb1f1db910687bbd658547e2ca0daae9f7
vadivel96 <107264117+vadivel96@users.noreply.github.com>
2023-05-20 19:32:13 -0700

    Add files via upload

---
+++ zen tasks/day 19(ns)/currency exchange.js
@@ -0,0 +1,87 @@
+
+
+
+function output(event){
+    event.preventDefault();
+      
+    let emptyarray=[];
+
+ 
+   async function httpcode() {
+
+    function currencycode1(){
+        return findingcurrencycode1()
+        async function findingcurrencycode1(){
+        let country=document.getElementById('typeCountry1').value;
+        console.log(country)
+        let restcountries=await fetch(`https://restcountries.com/v3.1/name/${country}`)
+        var country1=await restcountries.json();
+        var currencycode1=Object.keys(country1[0].currencies);
+        currencycode1= currencycode1[0].toLowerCase();
+        console.log(currencycode1);
+        emptyarray.push(currencycode1)
+        return currencycode1;
+        
+       
+    }
+         
+    }
+
+    
+
+    
+     function currencycode2(){
+    
+      return findingcurrencycode2()
+      async function findingcurrencycode2(){
+        let country=document.getElementById('typeCountry2').value;
+        let restcountries=await fetch(`https://restcountries.com/v3.1/name/${country}`)
+        var country2=await restcountries.json();
+        var currencycode2=Object.keys(country2[0].currencies);
+        currencycode2=currencycode2[0].toLowerCase();
+        console.log(currencycode2);
+        emptyarray.push(currencycode2)
+        return currencycode2
+        
+    }
+   
+}
+                                          
+        let a = await currencycode1();
+        console.log(a);
+        let b = await currencycode2();
+        console.log(b);
+      
+        let codes = await fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${b}/${a}.json`);
+        let httpcode1 = await codes.json();
+        console.log(httpcode1);
+       
+
+        let newdiv=document.querySelector(".new");
+        newdiv.innerHTML="";
+        let card=document.createElement('div');
+        card.innerHTML=`<div class="card" style="width: 18rem;">
+        <div class="card-body">
+        <h5 class="card-title">${document.getElementById('typeCountry1').value}</h5>
+        <h6 class="card-subtitle mb-2 text-muted">${document.getElementById('typeCountry2').value}</h6>
+        <p class="card-text">
+            ${httpcode1[a]} ${a}   equal to 1${b}
+            </br>
+        last updated:${httpcode1.date}
+        </p>
+
+        </div>
+
+        </div>`
+        newdiv.append(card);
+
+    }
+
+ 
+httpcode();
+
+
+
+}
+
+
